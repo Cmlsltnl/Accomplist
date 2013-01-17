c06386b4b42c612d8708af67b39ff78e119d2489
Neel <neel@ubuntu.(none)>
2013-01-17 00:18:39 -0600

    deploy

--- wonderevent/urls.py
+++ wonderevent/urls.py
@@ -58,7 +58,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^avatar/', include('avatar.urls')),
+    
 
     url(r'^messages/', include('messages.urls')),
 
